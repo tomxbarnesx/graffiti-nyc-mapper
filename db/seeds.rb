d2102b384ddba2f07f7c67fe23e702938322312f
Tom Barnes <barnes.thomas.c@gmail.com>
2018-09-26 14:39:18 -0400

    attempting heroku fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@
 # Piece.create(title: "Muscly Liger", lat: 40.705891, lng:-73.922410);
 
 User.create(first_name: "NY", last_name: "Admin", email: "admin@test.com", is_admin: true, birthdate: "19900313", bio: "Admin responsible for maintaing the NYC graffiti maps project.", username: "nyadmin", password: "password");
-User.create(first_name: "Frank", last_name: "Sinatra", email: "test@test.com", is_admin: false, birthdate: "19910313", bio: "Test account for the NYC graffiti maps project.", username: "frankiesinatra", password: "password");
+User.create(first_name: "Frank", last_name: "Sinatra", email: "test@test.com", is_admin: false, birthdate: "19910313", bio: "Test account for the NYC graffiti maps project. From the heart of the city, for the heart of the city.", username: "frankiesinatra", password: "password");
